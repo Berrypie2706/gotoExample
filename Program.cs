30a8cbdec1bc70210fd2d627482869dbb8118863
Berrypie2706 <aniket.chouksey444@gmail.com>
2023-07-11 12:10:17 +0530

    Second commit

--- Program.cs
+++ Program.cs
@@ -11,20 +11,25 @@ namespace GotoExample
     {
         static void Main(string[] args)
         {
-            string userName;
-            Again:
-            Console.WriteLine("Enter User Name");
-            userName = Console.ReadLine();
-            if (userName.Length >= 6)
-            {
-                Console.WriteLine("Welcome : \t " + userName);
-            }
-            else
-            {
-                Console.WriteLine("Invalid user");
-                goto Again;
-            }
+            //string userName;
+            //Again:
+            //Console.WriteLine("Enter User Name");
+            //userName = Console.ReadLine();
+            //if (userName.Length >= 6)
+            //{
+            //    Console.WriteLine("Welcome : \t " + userName);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid user");
+            //    goto Again;
+            //}
+            StringBuilder sb = new StringBuilder("Welcome");
+            sb.Append("Welcome to C#");
+            sb.Append("I am stringBuilder");
+            Console.WriteLine(sb);
             Console.ReadKey();
+
         }
     }
 }
