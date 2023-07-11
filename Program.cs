8da3e82de0ae8875627ed906650dad740d6b4c35
Berrypie2706 <aniket.chouksey444@gmail.com>
2023-07-11 12:35:10 +0530

    Third commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,12 +25,24 @@ namespace GotoExample
             //    Console.WriteLine("Invalid user");
             //    goto Again;
             //}
-            StringBuilder sb = new StringBuilder("Welcome");
-            sb.Append("Welcome to C#");
-            sb.Append("I am stringBuilder");
-            Console.WriteLine(sb);
-            Console.ReadKey();
 
+
+            //StringBuilder sb = new StringBuilder("Welcome");
+            //sb.Append("Welcome to C#");
+            //sb.Append("I am stringBuilder");
+            //Console.WriteLine(sb);
+            //Console.ReadKey();
+            
+            for(int i=1;i<10;i++)
+            {
+                if(i == 6)
+                {
+                    goto end;
+                }
+                Console.WriteLine("i value :{0}", i);
+            }
+        end: Console.WriteLine("The end");
+            Console.ReadKey();
         }
     }
 }
